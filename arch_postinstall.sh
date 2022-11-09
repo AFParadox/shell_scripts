06dad35c2ba74bd5f3494a80d375b1ac77925bd0
Leonardo Da Re <leonardodare@gmail.com>
2022-11-09 10:43:03 +0100

    Created first version of post install arch script

---
+++ arch_postinstall.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# System update
+yay
+
+# Programs
+yay -S brave-bin calibre chrome-gnome-shell discord droidcam git github-desktop libreoffice-fresh neofetch snapd spotify-adblock-git spotify-launcher steam sublime-text-4 timeshift visual-studio-code-bin vlc zoom
